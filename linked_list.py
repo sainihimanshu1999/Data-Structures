a99ffa7add9beba98da8f94e9610b6c759504a53
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-08-01 14:25:36 +0530

    merging sorted lists

--- linked_list.py
+++ linked_list.py
@@ -139,19 +139,72 @@ class LinkedList:
             cur = nxt
         self.head = prev
 
+    def merge_sorted(self, llist):
 
-llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
-llist.append("H")
-llist.append("S")
-llist.swap_node('A', 'C')
-# llist.insert_after_node(llist.head.next, "E")
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+        return new_head
+
+
+llist_1 = LinkedList()
+llist_2 = LinkedList()
+
+llist_1.append(1)
+llist_1.append(5)
+llist_1.append(7)
+llist_1.append(9)
+llist_1.append(10)
+
+llist_2.append(2)
+llist_2.append(3)
+llist_2.append(4)
+llist_2.append(6)
+llist_2.append(8)
+
+llist_1.merge_sorted(llist_2)
+llist_1.print_list()
+
+
+# llist = LinkedList()
+# llist.append("A")
+# llist.append("B")
+# llist.append("C")
+# llist.append("D")
+# llist.append("H")
+# llist.append("S")
+# llist.swap_node('A', 'C')
+# # llist.insert_after_node(llist.head.next, "E")
 # llist.append("E")
 # llist.delete_node_at_pos(2)
 # print(llist.len_iterative())
-llist.reverse_iterative()
-print(llist.len_recursive(llist.head))
-llist.print_list()
+# llist.reverse_iterative()
+# print(llist.len_recursive(llist.head))
+# llist.print_list()
