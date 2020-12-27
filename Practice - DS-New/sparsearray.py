71f6978bc393077c193d54890fc7c46b2d6a8ca5
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-12-27 14:04:10 +0530

    sparse arrays soltuion

---
+++ Practice - DS-New/sparsearray.py
@@ -0,0 +1,90 @@
+# METHOD 1 i.e traditional method
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    result = []
+    for i in range(len(queries)):
+        count = 0
+        for j in range(len(strings)):
+            if queries[i]==strings[j]:
+                count += 1
+        result.append(count)
+    return result
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+
+
+#METHOD 2 USING COUNT APPROACH
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    result = []
+    for q in queries:
+        result.append(strings.count(q))
+    return result
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
